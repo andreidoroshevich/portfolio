98e2f8815136223d0201b9facce65ea504db3c3a
a.n.darashevich <a.n.darashevich@gmail.com>
2022-06-03 15:40:36 +0300

    03.06.2022 (add working contact form)

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -2,23 +2,38 @@ import React from 'react';
 import s from './Contacts.module.scss'
 import styleContainer from "../../common/styles/Container.module.scss";
 import Title from "../../common/title/Title";
-import Button from "../../common/button/Button";
+import styles from "../../common/button/Button.module.scss"
+import emailjs from "emailjs-com"
+
 
 type ContactsPropsType = {
     id: string
 }
 
 function Contacts(props: ContactsPropsType) {
+
+    const sendEmail = (e: any)=>{
+        e.preventDefault()
+        emailjs.sendForm("service_bwpru7n", "template_5lw311h", e.target, "tSV--Lv2gsdM8aYLl")
+            .then((res)=>{
+                console.log(res.text)
+            }, (error)=>{
+                console.log(error.text)
+            })
+        e.target.reset()
+    }
+
     return (
         <div className={s.contactsBlock} id={props.id}>
             <div className={`${styleContainer.container} ${s.contactsContainer}`}>
                 <Title title={"Contacts"}/>
-                <form className={s.form}>
-                    <input placeholder={"Your name"} className={s.input} type={"text"}/>
-                    <input placeholder={"Your email"} className={s.input} type={"text"}/>
-                    <textarea placeholder={"Your message"} className={s.textarea}></textarea>
+                <form className={s.form} onSubmit={sendEmail}>
+                    <input placeholder={"Your name"} className={s.input} type={"text"} name={"user-name"}/>
+                    <input placeholder={"Your email"} className={s.input} type={"text"} name={"user-email"}/>
+                    <textarea placeholder={"Your message"} className={s.textarea} name={"message"}></textarea>
+                    <button type={'submit'} value={"Send"} className={styles.button}>Send</button>
+
                 </form>
-                <Button title={"Send"} callBack={()=>{}}/>
             </div>
 
         </div>
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -14,11 +14,21 @@ function Main(props: MainPropsType) {
                     <span>Hi there!</span>
                     <h1>I am Andrei Darashevich</h1>
                     <p>
-                        Front-end Developer
+                        Front-End developer
+                    </p>
+
+                    <p className={s.profile}>
+                        Front-End developer with experience in creating SPA using React, Redux, Typescript, HTML, CSS/SCSS, JS.
+                    </p>
+                    <p className={s.profile}>
+                        Also I have an interest in improving my English.
+                    </p>
+                    <p className={s.profile}>
+                        Able to effectively self-manage during independent projects, as well as collaborate as part of a productive team.
                     </p>
                 </div>
                 <div className={s.photo}>
-                    Here will be photo
+                    Here will be my photo
                 </div>
             </div>
         </div>
